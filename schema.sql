7d7aad2176e9c93c3d75496904847843695e4e53
Anuj <anujagrawal948@gmail.com>
2026-05-09 17:29:45 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,25 +1,208 @@
--- Database Schema for Smart Task Manager System
+--
+-- PostgreSQL database dump
+--
 
--- 1. Users Table
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(200) NOT NULL
+\restrict mpyL0D6AF4GsOn72y96G7FcPJSAcyCR2E1Ya8kY4i79Ap0Yoacp94g2uM2whgWL
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+-- Started on 2026-05-09 17:27:35
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 222 (class 1259 OID 16402)
+-- Name: task; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.task (
+    id integer NOT NULL,
+    title character varying(100) NOT NULL,
+    description text,
+    priority character varying(20) NOT NULL,
+    status character varying(20) NOT NULL,
+    created_date timestamp without time zone,
+    user_id integer NOT NULL
 );
 
--- 2. Tasks Table
-CREATE TABLE task (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    description TEXT,
-    priority VARCHAR(20) NOT NULL DEFAULT 'Medium',
-    status VARCHAR(20) NOT NULL DEFAULT 'Pending',
-    created_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    
-    -- Foreign Key linking the task to the user who created it
-    user_id INTEGER NOT NULL,
-    CONSTRAINT fk_user
-      FOREIGN KEY(user_id) 
-      REFERENCES "user"(id)
-      ON DELETE CASCADE
+
+ALTER TABLE public.task OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 16401)
+-- Name: task_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.task_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.task_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 5026 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: task_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.task_id_seq OWNED BY public.task.id;
+
+
+--
+-- TOC entry 220 (class 1259 OID 16390)
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."user" (
+    id integer NOT NULL,
+    username character varying(50) NOT NULL,
+    password character varying(200) NOT NULL
 );
+
+
+ALTER TABLE public."user" OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 16389)
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.user_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 5027 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
+
+
+--
+-- TOC entry 4862 (class 2604 OID 16405)
+-- Name: task id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.task ALTER COLUMN id SET DEFAULT nextval('public.task_id_seq'::regclass);
+
+
+--
+-- TOC entry 4861 (class 2604 OID 16393)
+-- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+
+
+--
+-- TOC entry 5020 (class 0 OID 16402)
+-- Dependencies: 222
+-- Data for Name: task; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.task (id, title, description, priority, status, created_date, user_id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 5018 (class 0 OID 16390)
+-- Dependencies: 220
+-- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."user" (id, username, password) FROM stdin;
+1	Anuj	scrypt:32768:8:1$jL2MpYiRJ4n1bUZs$c3a07d19322798f02e120244a78426fb7b1e636b45dfad0c6a302f8dae0334f81a0498f2f30c6c21698f805d6052eeae43333b6fa23febfe538a1c1f558ad4e6
+\.
+
+
+--
+-- TOC entry 5028 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: task_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.task_id_seq', 14, true);
+
+
+--
+-- TOC entry 5029 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.user_id_seq', 1, true);
+
+
+--
+-- TOC entry 4868 (class 2606 OID 16414)
+-- Name: task task_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.task
+    ADD CONSTRAINT task_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4864 (class 2606 OID 16398)
+-- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4866 (class 2606 OID 16400)
+-- Name: user user_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_username_key UNIQUE (username);
+
+
+--
+-- TOC entry 4869 (class 2606 OID 16415)
+-- Name: task task_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.task
+    ADD CONSTRAINT task_user_id_fkey FOREIGN KEY (user_id) REFERENCES public."user"(id);
+
+
+-- Completed on 2026-05-09 17:27:36
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict mpyL0D6AF4GsOn72y96G7FcPJSAcyCR2E1Ya8kY4i79Ap0Yoacp94g2uM2whgWL
+
